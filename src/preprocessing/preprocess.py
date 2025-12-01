5a1660d01b22b6458f28e1eb07d85886acf0c4e2
Luqman-234 <elmiluqman@gmail.com>
2025-11-30 23:41:39 -0500

    Updated preprocess
    -Added multi line functionality for strings and some comments
    
    -Added a case for "/* - */" like comments
    
    -Fixed an issue with //

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,7 +1,11 @@
 # preprocessing/normalize.py
 # Python program which takes a whole file and outputs a list of normalized lines
 # strip, collapse spaces, maybe lowercase, maybe remove comments
-# Additionally, there is an option to put everything in lowercase
+# Puts everything in lowercase by default
+
+
+string_mode = False  # Global variable, allowing strings to be multi line
+comment_mode = False # Global variable, allowing comments like /* */ to be multi line
 
 
 def normalize_line(line: str, lowercase: bool) -> str:
@@ -10,31 +14,34 @@ def normalize_line(line: str, lowercase: bool) -> str:
         them into the list Tokens(new line is not included, not sure if that is desireable or not)
         It sends back an empty string if there are no tokens
         otherwise it loops through the tokens list and makes space between each
-
-        A pretty apparent weakness is the lack of communication between lines, especially if there is a comment spanning
-        multiple lines.
     """
+    global string_mode  # Declare intent to modify the global variable
+    global comment_mode  # Declare intent to modify the global variable
     if line == "":
         return ""
-    tokens = [] # makes a list of tokens that will separated with space
-    token = "" # an element of tokens
-    new_line = "" #the output of this method
-    prev_quotation= '' # makes sure that the same initial quotation is being used
-    prev_char = "" # Used to see what the previous character in the string was
-    string_mode = False # This mode is for reading strings
-    operators = {"+", "-", "*", "/", "%", "!", ">", "<", ":", ";", "(", ")", "[", ']', '{', '}'} # list of characters we want as individual tokens
-    for char in line: #go through each character of line
-        if char == '\n': #like I said, new line character is excluded
+    list_length = len(line) #length of line
+    tokens = []  # makes a list of tokens that will separated with space
+    token = ""  # an element of tokens
+    new_line = ""  # the output of this method
+    prev_quotation = ''  # makes sure that the same initial quotation is being used
+    prev_char = ""  # Used to see what the previous character in the string was
+    operators = {"+", "-", "*", "/", "%", "!", ">", "<", ":", ";", "(", ")", "[", ']', '{', '}'}  # list of characters we want as individual tokens
+    for index, char in enumerate(line):  # go through each character of line
+        if char == '\n':  # like I said, new line character is excluded
             if token != '':
                 tokens.append(token)
             break
-        if string_mode: #string mode, the loop will stay here until the same quotation marks appear
+        if string_mode:  # string mode, the loop will stay here until the same quotation marks appear
             token += char
             if char == prev_quotation:
                 string_mode = False
                 tokens.append(token)
                 token = ''
-        else: #the normal loop
+        elif comment_mode:
+            if prev_char == '*' and char == '/':
+                comment_mode = False
+                prev_char = ''
+        else:  # the normal loop
             if char == "\"" or char == "'":  # Checks if a quotation mark forms to turn into string mode
                 if token != "":
                     tokens.append(token)
@@ -42,25 +49,32 @@ def normalize_line(line: str, lowercase: bool) -> str:
                 string_mode = True
                 prev_quotation = char
                 token += char
-            elif char == " ": # checks for a space character, ignores it, only really using it to isolate tokens
+            elif char == " ":  # checks for a space character, ignores it, only really using it to isolate tokens
                 if token != "":
                     tokens.append(token)
                     token = ''
-            elif char == "#" or (char == "/" and prev_char == "/"): #checks if it is a comment, if so, exits the loop
+            elif char == "#" or index < list_length -1 and (char == "/" and line[index+1] == "/"):  # checks if it is a comment, if so, exits the loop
                 break
-            elif  char in operators: #checks if it is an operator, if so tokenize it individually
+            elif index < list_length -1 and char == "/" and line[index+1] == "*": #checks if the line has /*
+                if token != '':
+                    tokens.append(token)
+                token = ''
+                comment_mode = True
+                prev_char = ""
+                continue
+            elif char in operators:  # checks if it is an operator, if so tokenize it individually
                 if token != '':
                     tokens.append(token)
                 token = ''
                 tokens.append(char)
-            else: # basic case, will lowercase it if the lowercase option is turned on
+            else:  # basic case, will lowercase it if the lowercase option is turned on
                 if lowercase and char.isupper():  # converts uppercase into lowercase
                     char = char.lower()
                 token += char
         prev_char = char
-    if tokens == []: #checks for empty list and returns an empty string if so
+    if tokens == []:  # checks for empty list and returns an empty string if so
         return ""
-    for index, token in enumerate(tokens): # Combine each string and seperate them with space character
+    for index, token in enumerate(tokens):  # Combine each string and seperate them with space character
         if index == 0:
             new_line += token
             continue
@@ -69,20 +83,13 @@ def normalize_line(line: str, lowercase: bool) -> str:
         new_line += ' ' + token
     return new_line
 
-def normalize_file(path: str, lowercase: bool) -> list[str]: #reads from a file and makes a list of lines in the file
-    normalize_list = [] #output of method
-    with open(path) as f: #opens file
-        lines = f.readlines() #makes the lines list
-    for l in lines: #basic loop that gives each line to normalize_line() and combines them into a list of non empty strings
-        temp = normalize_line(l, lowercase)
+
+def normalize_file(path: str) -> list[str]:  # reads from a file and makes a list of lines in the file
+    normalize_list = []  # output of method
+    with open(path) as f:  # opens file
+        lines = f.readlines()  # makes the lines list
+    for l in lines:  # basic loop that gives each line to normalize_line() and combines them into a list of non empty strings
+        temp = normalize_line(l, True)
         if temp != "":
             normalize_list.append(temp)
     return normalize_list
-
-def test():
-    temp = normalize_file(".venv\\Temp.txt", False)
-    for l in temp:
-        print(l)
-
-if __name__ == "__main__":
-    test()
